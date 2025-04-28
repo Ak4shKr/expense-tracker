49b5fb885d1e3bab809a35b589db39b4f6b85a80
Ak4shKr <akashkmr7667@gmail.com>
2025-04-28 22:19:15 +0530

    base url updated

--- frontend/src/services/service.js
+++ frontend/src/services/service.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const service = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  // baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://expense-tracker-oyua.onrender.com/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
